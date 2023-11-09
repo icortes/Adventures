4d357ee9fe9cbb350db872c68a3d0be2938fbc3d
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-11-09 14:16:00 -0800

    table shows with data

--- scripts/app.js
+++ scripts/app.js
@@ -11,152 +11,45 @@ function fillActivityCategories(activityCategory) {
   });
 }
 
-/**
- * Listens for change event in activityCategory and fill activities
- * dropdown with activities with same category property.
- * @param {HTMLElement} activityCategory
- * @param {HTMLElement} activityNameGroup
- * @param {HTMLElement} activityName
- * @param {HTMLElement} activityFooter
- */
-function showActivitiesAndFill(
-  activityCategory,
-  activityNameGroup,
-  activityName,
-  activityFooter
-) {
-  // get value of selected category
-  let selectedCategory = activityCategory.value;
-  // change event listener for category dropdown
+function onCategorySelect(activityCategory) {
+  let activitiesDisplay = document.getElementById('activitiesDisplay');
+  let tbody = document.getElementById('activitiesTableBody');
   activityCategory.addEventListener('change', () => {
-    // get new value of selected category
-    selectedCategory = activityCategory.value;
-    console.log(`Activity Category Selected: ${activityCategory.value}`);
-    // hide element whenever new category is selected
-    hideElement(activityFooter);
-    // if selected category has a value
-    if (selectedCategory != '') {
-      // show activity category
-      showElement(activityNameGroup);
-      // fill the activity dropdown with activity with same category property
-      fillBasedOnCategory(selectedCategory, activityName);
+    tbody.innerHTML = '';
+    console.log(`User selected: ${activityCategory.value}`);
+    if (activityCategory.value != '') {
+      //filter activities and only get objects that match same category value
+      let filteredActivities = activities.filter(
+        (activity) => activity.category == activityCategory.value
+      );
+      console.log(activityCategory.value + ' Activities');
+      console.table(filteredActivities);
+      //fill in table body with filteredActivities
+      filteredActivities.forEach((activity) => buildActivityRow(tbody, activity));
+      showElement(activitiesDisplay);
     } else {
-      //if selected category has no value hide the activity dropdown
-      hideElement(activityNameGroup);
+      hideElement(activitiesDisplay);
     }
   });
 }
 
-/**
- * Filters activities array and appends activities equal to the selected category.
- * @param {string} selectedCategory
- * @param {HTMLElement} activityName
- */
-function fillBasedOnCategory(selectedCategory, activityName) {
-  // every time activity dropdown fills, reset the children to only one option element
-  activityName.innerHTML = `<option value="" selected>Select One...</option>`;
-  // filter activities's categories that are equal to the selected category and return to new array
-  let filteredActivities = activities.filter((activity) => {
-    //condition to filter array
-    return activity.category == selectedCategory;
-  });
-  // for every item in the array
-  filteredActivities.forEach((activity) => {
-    // append new option element with activity name and id to activity dropdown
-    activityName.appendChild(new Option(activity.name, activity.id));
-  });
-}
-/**
- * When the user selects an activity, details of that activity (id, name, description, location, price,
- *will be displayed on the page. If the price is more than $0.00, the page will also display a
- *form with fields that can be used to buy an e-ticket.
- * @param {HTMLElement} activityName
- * @param {HTMLElement} activityFooter
- */
-function showSelectedActivityAndPaymentForm(activityName, activityFooter) {
-  // activity id
-  let activityId = activityName.value;
-  // empty object for selected activity
-  let selectedActivity = {};
-  // change event listener for activity dropdown
-  // whenever user selects new activity
-  activityName.addEventListener('change', () => {
-    // get new activity id
-    activityId = activityName.value;
-    console.log(`Activity Selected: ${activityId}`);
-    // if activity is not empty
-    if (activityId != '') {
-      // show information about activity
-      showElement(activityFooter);
-      // fill the card and return selected activity object
-      selectedActivity = fillCard(activityId);
-      console.log(`Selected Activity Name: ${selectedActivity.name}`);
-      //display payment if price is > 0
-      if (selectedActivity.price > 0) {
-        console.log(`Price: $${selectedActivity.price}`);
-        // hide confirmation element with confirmation text
-        hideElement(document.getElementById('confirmation'));
-        // display payment form and handle form submit
-        displayPaymentForm(selectedActivity.price, selectedActivity.name);
-      } else {
-        // if payment is 0 hide the payment form
-        hideElement(document.getElementById('payment-form'));
-      }
-    } else {
-      // hide activity information if no activity is selected
-      hideElement(activityFooter);
-    }
-  });
-}
-/**
- * Fills card content with selected activity information
- * @param {string} activity
- * @returns selected activity object
- */
-function fillCard(activityId) {
-  // filter activities and return activity equal to activity id
-  let selectedActivity = activities.filter((item) => {
-    return activityId == item.id;
-  })[0];
-
-  //get card elements and fill them with activity information
-  document.getElementById('card-name').textContent = selectedActivity.name;
-  document.getElementById('card-id').textContent = selectedActivity.id;
-  document.getElementById('card-title').textContent = selectedActivity.category;
-  document.getElementById('card-location').textContent = selectedActivity.location;
-  document.getElementById('card-price').textContent = `$${selectedActivity.price}`;
-  document.getElementById('card-description').textContent = selectedActivity.description;
-
-  //return activity object
-  return selectedActivity;
-}
-/**
- * Displays payment form and handles onSubmit event
- * @param {number} activityPrice
- * @param {string} activityName
- */
-function displayPaymentForm(activityPrice, activityName) {
-  let paymentForm = document.getElementById('payment-form');
-  // shoes payment form
-  showElement(paymentForm);
-  // submit event listener on form
-  paymentForm.addEventListener('submit', (event) => {
-    //prevent page from reloading
-    event.preventDefault();
-    //get form values from DOM
-    const numberTickets = +document.getElementById('number-tickets').value;
-    const email = document.getElementById('email').value;
-    // show confirmation message for buying tickets
-    showElement(document.getElementById('confirmation'));
-    // show message to DOM
-    document.getElementById(
-      'confirmation-text'
-    ).textContent = `Your credit card has been charged $${
-      activityPrice * numberTickets
-    } for ${numberTickets} to
-${activityName}. A confirmation email has been sent to ${email}.
-`;
-  });
+function buildActivityRow(tbody, activity) {
+  // Create an empty <tr> element and add it to the last
+  // position of the table
+  let row = tbody.insertRow(-1);
+  // Create new cells (<td> elements) and add text
+  let cellId = row.insertCell(0);
+  cellId.textContent = activity.id;
+  let cellCategory = row.insertCell(1);
+  cellCategory.textContent = activity.category;
+  let cellName = row.insertCell(2);
+  cellName.textContent = activity.name;
+  let cellDescription = row.insertCell(3);
+  cellDescription.textContent = activity.description;
+  let cellLocation = row.insertCell(4);
+  cellLocation.textContent = activity.location;
+  let cellPrice = row.insertCell(5);
+  cellPrice.textContent = '$' + activity.price;
 }
 
 /**
@@ -177,16 +70,7 @@ function showElement(element) {
 //run this code on page load
 onload = () => {
   let activityCategory = document.getElementById('activityCategory');
-  let activityNameGroup = document.getElementById('activityNameGroup');
-  let activityName = document.getElementById('activityName');
-  let activityFooter = document.getElementById('activity-footer');
 
   fillActivityCategories(activityCategory);
-  showActivitiesAndFill(
-    activityCategory,
-    activityNameGroup,
-    activityName,
-    activityFooter
-  );
-  showSelectedActivityAndPaymentForm(activityName, activityFooter);
+  onCategorySelect(activityCategory);
 };
